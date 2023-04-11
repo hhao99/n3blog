4a54b61622776bc6a87d877983b70ca5ae939567
hhao99 <hhao99@gmail.com>
2023-04-11 15:30:07 -0500

    add the md editor

--- src/pages/edit.tsx
+++ src/pages/edit.tsx
@@ -1,38 +1,24 @@
-import '@uiw/react-md-editor/markdown-editor.css'
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
 
 import dynamic from "next/dynamic";
-import { useState } from 'react'
+import { useState } from "react";
 
 const MDEditor = dynamic(
-    () => import("@uiw/react-md-editor").then((mod) => mod.default),
-    { ssr: false }
-  );
-  const EditorMarkdown = dynamic(
-    () =>
-      import("@uiw/react-md-editor").then((mod) => {
-        return mod.default.Markdown;
-      }),
-    { ssr: false }
-  );
+  () => import("@uiw/react-md-editor"),
+  { ssr: false }
+);
 export default function Editor() {
     const initialContent = `
-    # A demo a markdown
-    this is basic demo of the markedown editor
-    * add one line
-    * add two line
-    * add three line
-    ("https://www.github.com")[full reference please search @uid/react-md-editor]
-    `
+ ** simple markdown editor **
+ `
     const [content,setContent] = useState<string>(initialContent)
     return (
-        <div data-color-mode='dark'>
+        <div data-color-mode='light'>
             <MDEditor value={content} 
-                onChange={setContent}/>
-            <div style={{ paddingTop: 50}}>
-                <EditorMarkdown source={content} />
-            </div>
+                onChange={setContent}
+                />
+            
         </div>
     )
 }
